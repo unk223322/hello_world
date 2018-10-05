cd3b5df7efab88127a9931c35ee66a32f6244a84
unk223322 <unk223322@gmail.com>
2018-10-04 19:13:32 -0700

    Hello world code
    Hello world code

---
+++ hello_world.py
@@ -0,0 +1,106 @@
+
+
+
+
+
+
+
+
+
+print('Hello world yall!')
+
+x = 1
+y = False
+z = 'Yay!'
+a = [5, 10, 20]
+b = {0: 'Zero string', 1: 'One string'}
+
+print(x)
+print(y)
+print(z)
+print(a)
+print(b)
+
+
+if x == 0:
+    # Do stuff
+    print('x  is 0.')
+    print('Next line')
+elif x ==   1:
+    print('x is 1.')
+else:
+    print('x is othhhhher.')
+
+for element in a:
+    print(element)
+
+for i in range(0,len(a)):
+    print(a[i])
+
+
+def upperC(text='Hhhhhello'):
+    print(text.upper())
+    return 'success'
+
+print(upperC())
+
+print(b.values())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
